2a5898d431d1c729c8228944c26ba42410bef6dd
Balagur Dmitry <farnitin@yandex.ru>
2025-03-22 05:31:29 +0400

    add start of application

---
+++ src/main/java/ru/shalter/ShelterPluzaApplication.java
@@ -0,0 +1,11 @@
+package ru.shalter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterPluzaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShelterPluzaApplication.class, args);
+    }
+}
